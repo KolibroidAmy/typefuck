50072d04e3268ed436881fa13d3cce06870e9063
koli <kolibroidamy@gmail.com>
2025-01-26 15:15:30 +0000

    Input/Output (+.gitignore)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::any::type_name;
 use std::marker::PhantomData;
 
 enum Uninhabited {}
@@ -63,12 +62,16 @@ where
     type Write<Val> = TDoubleStack<Left, <Right as TInfStack<D>>::Set<Val>>;
 }
 
+struct IOState<Input, Output>(PhantomData<Input>, PhantomData<Output>, Uninhabited);
+
 struct MoveRight(Uninhabited);
 struct MoveLeft(Uninhabited);
 struct Increment(Uninhabited);
 struct Decrement(Uninhabited);
 struct EndLoop(Uninhabited);
 struct StartLoop(Uninhabited);
+struct Getch(Uninhabited);
+struct Putch(Uninhabited);
 struct Halt(Uninhabited);
 
 struct FetchGoingBack<Depth>(PhantomData<Depth>, Uninhabited);
@@ -77,187 +80,254 @@ struct Execute<Inst>(PhantomData<Inst>, Uninhabited);
 struct Fetch(Uninhabited);
 struct LoopCompare<Num>(PhantomData<Num>, Uninhabited);
 
-struct InterpreterState<Code: InfTape<Halt>, Mem: InfTape<Zero>, State>(
+struct InterpreterState<
+    Code: InfTape<Halt>,
+    Mem: InfTape<Zero>,
+    In: InfTape<Zero>,
+    Out: InfTape<Zero>,
+    State,
+>(
     PhantomData<Code>,
     PhantomData<Mem>,
+    PhantomData<In>,
+    PhantomData<Out>,
     PhantomData<State>,
     Uninhabited,
 );
 
 trait Interpreter {
-    type FinalState;
+    type Output;
 }
 
-type IRec<I: Interpreter> = <I as Interpreter>::FinalState;
+type IRec<I: Interpreter> = <I as Interpreter>::Output;
 
 // Fetch
-impl<Code: InfTape<Halt>, Mem: InfTape<Zero>> Interpreter for InterpreterState<Code, Mem, Fetch>
+impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, In: InfTape<Zero>, Out: InfTape<Zero>> Interpreter
+    for InterpreterState<Code, Mem, In, Out, Fetch>
 where
-    InterpreterState<Code, Mem, Execute<Code::Read>>: Interpreter,
+    InterpreterState<Code, Mem, In, Out, Execute<Code::Read>>: Interpreter,
 {
-    type FinalState = IRec<InterpreterState<Code, Mem, Execute<Code::Read>>>;
+    type Output = IRec<InterpreterState<Code, Mem, In, Out, Execute<Code::Read>>>;
 }
 
 // Halt
-impl<Code: InfTape<Halt>, Mem: InfTape<Zero>> Interpreter
-    for InterpreterState<Code, Mem, Execute<Halt>>
+impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, In: InfTape<Zero>, Out: InfTape<Zero>> Interpreter
+    for InterpreterState<Code, Mem, In, Out, Execute<Halt>>
 {
-    type FinalState = Self;
+    type Output = Out;
 }
 
 // Move right
-impl<Code: InfTape<Halt>, Mem: InfTape<Zero>> Interpreter
-    for InterpreterState<Code, Mem, Execute<MoveRight>>
+impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, In: InfTape<Zero>, Out: InfTape<Zero>> Interpreter
+    for InterpreterState<Code, Mem, In, Out, Execute<MoveRight>>
 where
-    InterpreterState<Code::MoveRight, Mem::MoveRight, Fetch>: Interpreter,
+    InterpreterState<Code::MoveRight, Mem::MoveRight, In, Out, Fetch>: Interpreter,
 {
-    type FinalState = IRec<InterpreterState<Code::MoveRight, Mem::MoveRight, Fetch>>;
+    type Output = IRec<InterpreterState<Code::MoveRight, Mem::MoveRight, In, Out, Fetch>>;
 }
 
 // Move left
-impl<Code: InfTape<Halt>, Mem: InfTape<Zero>> Interpreter
-    for InterpreterState<Code, Mem, Execute<MoveLeft>>
+impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, In: InfTape<Zero>, Out: InfTape<Zero>> Interpreter
+    for InterpreterState<Code, Mem, In, Out, Execute<MoveLeft>>
 where
-    InterpreterState<Code::MoveRight, Mem::MoveLeft, Fetch>: Interpreter,
+    InterpreterState<Code::MoveRight, Mem::MoveLeft, In, Out, Fetch>: Interpreter,
 {
-    type FinalState = IRec<InterpreterState<Code::MoveRight, Mem::MoveLeft, Fetch>>;
+    type Output = IRec<InterpreterState<Code::MoveRight, Mem::MoveLeft, In, Out, Fetch>>;
 }
 
 // Increment
-impl<Code: InfTape<Halt>, Mem: InfTape<Zero>> Interpreter
-    for InterpreterState<Code, Mem, Execute<Increment>>
+impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, In: InfTape<Zero>, Out: InfTape<Zero>> Interpreter
+    for InterpreterState<Code, Mem, In, Out, Execute<Increment>>
 where
     Mem::Read: TNumber,
-    InterpreterState<Code::MoveRight, Mem::Write<<Mem::Read as TNumber>::Incr>, Fetch>: Interpreter,
+    InterpreterState<Code::MoveRight, Mem::Write<<Mem::Read as TNumber>::Incr>, In, Out, Fetch>:
+        Interpreter,
 {
-    type FinalState =
-        IRec<InterpreterState<Code::MoveRight, Mem::Write<<Mem::Read as TNumber>::Incr>, Fetch>>;
+    type Output = IRec<
+        InterpreterState<Code::MoveRight, Mem::Write<<Mem::Read as TNumber>::Incr>, In, Out, Fetch>,
+    >;
 }
 
 // Decrement
-impl<Code: InfTape<Halt>, Mem: InfTape<Zero>> Interpreter
-    for InterpreterState<Code, Mem, Execute<Decrement>>
+impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, In: InfTape<Zero>, Out: InfTape<Zero>> Interpreter
+    for InterpreterState<Code, Mem, In, Out, Execute<Decrement>>
 where
     Mem::Read: TNumber,
-    InterpreterState<Code::MoveRight, Mem::Write<<Mem::Read as TNumber>::Decr>, Fetch>: Interpreter,
+    InterpreterState<Code::MoveRight, Mem::Write<<Mem::Read as TNumber>::Decr>, In, Out, Fetch>:
+        Interpreter,
 {
-    type FinalState =
-        IRec<InterpreterState<Code::MoveRight, Mem::Write<<Mem::Read as TNumber>::Decr>, Fetch>>;
+    type Output = IRec<
+        InterpreterState<Code::MoveRight, Mem::Write<<Mem::Read as TNumber>::Decr>, In, Out, Fetch>,
+    >;
+}
+
+// Getch
+impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, In: InfTape<Zero>, Out: InfTape<Zero>> Interpreter
+    for InterpreterState<Code, Mem, In, Out, Execute<Getch>>
+where
+    InterpreterState<Code::MoveRight, Mem::Write<In::Read>, In::MoveRight, Out, Fetch>: Interpreter,
+{
+    type Output =
+        IRec<InterpreterState<Code::MoveRight, Mem::Write<In::Read>, In::MoveRight, Out, Fetch>>;
+}
+
+// Putch
+impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, In: InfTape<Zero>, Out: InfTape<Zero>> Interpreter
+    for InterpreterState<Code, Mem, In, Out, Execute<Putch>>
+where
+    InterpreterState<Code::MoveRight, Mem, In, <Out::Write<Mem::Read> as InfTape<Zero>>::MoveRight, Fetch>:
+        Interpreter,
+{
+    type Output =
+        IRec<InterpreterState<Code::MoveRight, Mem, In, <Out::Write<Mem::Read> as InfTape<Zero>>::MoveRight, Fetch>>;
 }
 
 // Execute startloop (no-op)
-impl<Code: InfTape<Halt>, Mem: InfTape<Zero>> Interpreter
-    for InterpreterState<Code, Mem, Execute<StartLoop>>
+impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, In: InfTape<Zero>, Out: InfTape<Zero>> Interpreter
+    for InterpreterState<Code, Mem, In, Out, Execute<StartLoop>>
 where
-    InterpreterState<Code::MoveRight, Mem, Fetch>: Interpreter,
+    InterpreterState<Code::MoveRight, Mem, In, Out, Fetch>: Interpreter,
 {
-    type FinalState = IRec<InterpreterState<Code::MoveRight, Mem, Fetch>>;
+    type Output = IRec<InterpreterState<Code::MoveRight, Mem, In, Out, Fetch>>;
 }
 
 // Execute endloop (begin compare)
-impl<Code: InfTape<Halt>, Mem: InfTape<Zero>> Interpreter
-    for InterpreterState<Code, Mem, Execute<EndLoop>>
+impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, In: InfTape<Zero>, Out: InfTape<Zero>> Interpreter
+    for InterpreterState<Code, Mem, In, Out, Execute<EndLoop>>
 where
-    InterpreterState<Code, Mem, LoopCompare<Mem::Read>>: Interpreter,
+    InterpreterState<Code, Mem, In, Out, LoopCompare<Mem::Read>>: Interpreter,
 {
-    type FinalState = IRec<InterpreterState<Code, Mem, LoopCompare<Mem::Read>>>;
+    type Output = IRec<InterpreterState<Code, Mem, In, Out, LoopCompare<Mem::Read>>>;
 }
 
 // Loop value != 0 (begin loop)
-impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, I> Interpreter
-    for InterpreterState<Code, Mem, LoopCompare<Succ<I>>>
+impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, In: InfTape<Zero>, Out: InfTape<Zero>, I> Interpreter
+    for InterpreterState<Code, Mem, In, Out, LoopCompare<Succ<I>>>
 where
-    InterpreterState<Code::MoveLeft, Mem, FetchGoingBack<Zero>>: Interpreter,
+    InterpreterState<Code::MoveLeft, Mem, In, Out, FetchGoingBack<Zero>>: Interpreter,
 {
-    type FinalState = IRec<InterpreterState<Code::MoveLeft, Mem, FetchGoingBack<Zero>>>;
+    type Output = IRec<InterpreterState<Code::MoveLeft, Mem, In, Out, FetchGoingBack<Zero>>>;
 }
 
 // Loop value == 0 (no-op)
-impl<Code: InfTape<Halt>, Mem: InfTape<Zero, Read = Zero>> Interpreter
-    for InterpreterState<Code, Mem, LoopCompare<Zero>>
+impl<
+        Code: InfTape<Halt>,
+        Mem: InfTape<Zero, Read = Zero>,
+        In: InfTape<Zero>,
+        Out: InfTape<Zero>,
+    > Interpreter for InterpreterState<Code, Mem, In, Out, LoopCompare<Zero>>
 where
-    InterpreterState<Code::MoveRight, Mem, Fetch>: Interpreter,
+    InterpreterState<Code::MoveRight, Mem, In, Out, Fetch>: Interpreter,
 {
-    type FinalState = IRec<InterpreterState<Code::MoveRight, Mem, Fetch>>;
+    type Output = IRec<InterpreterState<Code::MoveRight, Mem, In, Out, Fetch>>;
 }
 
-impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, Depth> Interpreter
-    for InterpreterState<Code, Mem, FetchGoingBack<Depth>>
+impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, In: InfTape<Zero>, Out: InfTape<Zero>, Depth>
+    Interpreter for InterpreterState<Code, Mem, In, Out, FetchGoingBack<Depth>>
 where
-    InterpreterState<Code, Mem, GoingBack<Code::Read, Depth>>: Interpreter,
+    InterpreterState<Code, Mem, In, Out, GoingBack<Code::Read, Depth>>: Interpreter,
 {
-    type FinalState = IRec<InterpreterState<Code, Mem, GoingBack<Code::Read, Depth>>>;
+    type Output = IRec<InterpreterState<Code, Mem, In, Out, GoingBack<Code::Read, Depth>>>;
 }
 
 // Go back to startloop (depth=0, return to execution)
-impl<Code: InfTape<Halt>, Mem: InfTape<Zero>> Interpreter
-    for InterpreterState<Code, Mem, GoingBack<StartLoop, Zero>>
+impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, In: InfTape<Zero>, Out: InfTape<Zero>> Interpreter
+    for InterpreterState<Code, Mem, In, Out, GoingBack<StartLoop, Zero>>
 where
-    InterpreterState<Code::MoveRight, Mem, Fetch>: Interpreter,
+    InterpreterState<Code::MoveRight, Mem, In, Out, Fetch>: Interpreter,
 {
-    type FinalState = IRec<InterpreterState<Code::MoveRight, Mem, Fetch>>;
+    type Output = IRec<InterpreterState<Code::MoveRight, Mem, In, Out, Fetch>>;
 }
 
 // Go back to startloop (depth>0, decrease depth)
-impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, I> Interpreter
-    for InterpreterState<Code, Mem, GoingBack<StartLoop, Succ<I>>>
+impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, In: InfTape<Zero>, Out: InfTape<Zero>, I> Interpreter
+    for InterpreterState<Code, Mem, In, Out, GoingBack<StartLoop, Succ<I>>>
 where
-    InterpreterState<Code::MoveLeft, Mem, FetchGoingBack<I>>: Interpreter,
+    InterpreterState<Code::MoveLeft, Mem, In, Out, FetchGoingBack<I>>: Interpreter,
 {
-    type FinalState = IRec<InterpreterState<Code::MoveLeft, Mem, FetchGoingBack<I>>>;
+    type Output = IRec<InterpreterState<Code::MoveLeft, Mem, In, Out, FetchGoingBack<I>>>;
 }
 
 // Go back to endloop (increase depth)
-impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, I> Interpreter
-    for InterpreterState<Code, Mem, GoingBack<EndLoop, I>>
+impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, In: InfTape<Zero>, Out: InfTape<Zero>, I> Interpreter
+    for InterpreterState<Code, Mem, In, Out, GoingBack<EndLoop, I>>
 where
-    InterpreterState<Code::MoveLeft, Mem, FetchGoingBack<Succ<I>>>: Interpreter,
+    InterpreterState<Code::MoveLeft, Mem, In, Out, FetchGoingBack<Succ<I>>>: Interpreter,
 {
-    type FinalState = IRec<InterpreterState<Code::MoveLeft, Mem, FetchGoingBack<Succ<I>>>>;
+    type Output = IRec<InterpreterState<Code::MoveLeft, Mem, In, Out, FetchGoingBack<Succ<I>>>>;
 }
 
 // Go back to halt (code overran, halt)
-impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, I> Interpreter
-    for InterpreterState<Code, Mem, GoingBack<Halt, I>>
+impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, In: InfTape<Zero>, Out: InfTape<Zero>, I> Interpreter
+    for InterpreterState<Code, Mem, In, Out, GoingBack<Halt, I>>
 {
-    type FinalState = InterpreterState<Code::MoveLeft, Mem, FetchGoingBack<I>>;
+    type Output = Out;
 }
 
 // Go back to increment, decrement, move right, move left (all no-op)
-impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, I> Interpreter
-    for InterpreterState<Code, Mem, GoingBack<Increment, I>>
+impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, In: InfTape<Zero>, Out: InfTape<Zero>, I> Interpreter
+    for InterpreterState<Code, Mem, In, Out, GoingBack<Increment, I>>
 where
-    InterpreterState<Code::MoveLeft, Mem, FetchGoingBack<I>>: Interpreter,
+    InterpreterState<Code::MoveLeft, Mem, In, Out, FetchGoingBack<I>>: Interpreter,
 {
-    type FinalState = IRec<InterpreterState<Code::MoveLeft, Mem, FetchGoingBack<I>>>;
+    type Output = IRec<InterpreterState<Code::MoveLeft, Mem, In, Out, FetchGoingBack<I>>>;
 }
-impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, I> Interpreter
-    for InterpreterState<Code, Mem, GoingBack<Decrement, I>>
+impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, In: InfTape<Zero>, Out: InfTape<Zero>, I> Interpreter
+    for InterpreterState<Code, Mem, In, Out, GoingBack<Decrement, I>>
 where
-    InterpreterState<Code::MoveLeft, Mem, FetchGoingBack<I>>: Interpreter,
+    InterpreterState<Code::MoveLeft, Mem, In, Out, FetchGoingBack<I>>: Interpreter,
 {
-    type FinalState = IRec<InterpreterState<Code::MoveLeft, Mem, FetchGoingBack<I>>>;
+    type Output = IRec<InterpreterState<Code::MoveLeft, Mem, In, Out, FetchGoingBack<I>>>;
 }
-impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, I> Interpreter
-    for InterpreterState<Code, Mem, GoingBack<MoveRight, I>>
+impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, In: InfTape<Zero>, Out: InfTape<Zero>, I> Interpreter
+    for InterpreterState<Code, Mem, In, Out, GoingBack<MoveRight, I>>
 where
-    InterpreterState<Code::MoveLeft, Mem, FetchGoingBack<I>>: Interpreter,
+    InterpreterState<Code::MoveLeft, Mem, In, Out, FetchGoingBack<I>>: Interpreter,
 {
-    type FinalState = IRec<InterpreterState<Code::MoveLeft, Mem, FetchGoingBack<I>>>;
+    type Output = IRec<InterpreterState<Code::MoveLeft, Mem, In, Out, FetchGoingBack<I>>>;
 }
-impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, I> Interpreter
-    for InterpreterState<Code, Mem, GoingBack<MoveLeft, I>>
+impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, In: InfTape<Zero>, Out: InfTape<Zero>, I> Interpreter
+    for InterpreterState<Code, Mem, In, Out, GoingBack<MoveLeft, I>>
 where
-    InterpreterState<Code::MoveLeft, Mem, FetchGoingBack<I>>: Interpreter,
+    InterpreterState<Code::MoveLeft, Mem, In, Out, FetchGoingBack<I>>: Interpreter,
 {
-    type FinalState = IRec<InterpreterState<Code::MoveLeft, Mem, FetchGoingBack<I>>>;
+    type Output = IRec<InterpreterState<Code::MoveLeft, Mem, In, Out, FetchGoingBack<I>>>;
+}
+impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, In: InfTape<Zero>, Out: InfTape<Zero>, I> Interpreter
+    for InterpreterState<Code, Mem, In, Out, GoingBack<Getch, I>>
+where
+    InterpreterState<Code::MoveLeft, Mem, In, Out, FetchGoingBack<I>>: Interpreter,
+{
+    type Output = IRec<InterpreterState<Code::MoveLeft, Mem, In, Out, FetchGoingBack<I>>>;
+}
+impl<Code: InfTape<Halt>, Mem: InfTape<Zero>, In: InfTape<Zero>, Out: InfTape<Zero>, I> Interpreter
+    for InterpreterState<Code, Mem, In, Out, GoingBack<Putch, I>>
+where
+    InterpreterState<Code::MoveLeft, Mem, In, Out, FetchGoingBack<I>>: Interpreter,
+{
+    type Output = IRec<InterpreterState<Code::MoveLeft, Mem, In, Out, FetchGoingBack<I>>>;
 }
 
-type Run<Program> =
-    IRec<InterpreterState<TDoubleStack<Nil, Program>, TDoubleStack<Nil, Nil>, Fetch>>;
+type Run<Program, Input> = IRec<
+    InterpreterState<
+        TDoubleStack<Nil, Program>,
+        TDoubleStack<Nil, Nil>,
+        TDoubleStack<Nil, Input>,
+        TDoubleStack<Nil, Nil>,
+        Fetch,
+    >,
+>;
 
-type Program = Cons<Increment, Cons<MoveRight, Cons<Increment, Cons<Increment, Cons<StartLoop, Cons<Decrement, Cons<EndLoop, Nil>>>>>>>;
+type Program = Cons<Getch, Cons<StartLoop, Cons<Putch, Cons<Getch, Cons<EndLoop, Nil>>>>>;
+type Input = Cons<Succ<Succ<Succ<Zero>>>, Cons<Succ<Succ<Zero>>, Nil>>;
+
+type FinalOutput = Run<Program, Input>;
+
+
+trait Unimplemented {}
+fn cause_type_error<A: Unimplemented>() {}
+
 
 fn main() {
-    println!("{}", type_name::<Run<Program>>());
+    cause_type_error::<FinalOutput>()
 }
